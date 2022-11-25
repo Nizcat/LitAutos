1ea9a26db924c24c1df962bf3ea259eb2a86043f
Nizcat <nizalli@gmail.com>
2022-11-24 18:47:48 -0600

    show the autos with a random image

--- components/show-autos.js
+++ components/show-autos.js
@@ -7,12 +7,15 @@ export default class ShowAutos extends LitElement {
     year: { type: String },
     brand: { type: String },
     version: { type: String },
+    myArray: {type: Array},
+    rand: { type: Number },
   };
 
   constructor() {
     super();
     this.auto = [{}];
     this.saveData();
+    this.pickAnImage();
   }
  
 
@@ -25,31 +28,38 @@ export default class ShowAutos extends LitElement {
   ];
 
   render() {
-    return html` <div>${this.saveData()}</div>
-    <div>
+    return html` <div>${this.saveData(), this.pickAnImage()}</div>
+    <div class="" >
     ${this.auto.map(eachAuto => 
     html`
   <div class= "card" >
       <div class= "card-content">
           <h2>${eachAuto.name}</h2>
           <p>${eachAuto.brand}</p>
+          <img src=${eachAuto.img}>
       </div>
 
   </div>
   `)}
     </div>
 
-      }`;
+      `;
   }
   saveData() {
-    console.log(this.name, this.year, "las variables");
     this.auto.push({
       name: this.name,
       year: this.year,
       brand: this.brand,
       version: this.version,
+      img: this.imageAuto,
     });
     console.log(this.auto);
   }
+  pickAnImage(){
+  this.myArray = ['https://i.pinimg.com/474x/9a/a0/5d/9aa05dfc9d9a0c0b1a32ab39a7df3a8e.jpg', 'https://i.pinimg.com/736x/92/71/60/927160803cfd068253662a304cb2efff--chevrolet-auto-chevrolet-caprice.jpg', 'https://i.pinimg.com/originals/77/ef/84/77ef8486b48c628be8f0c8316a4a6139.jpg', 'https://i.pinimg.com/originals/40/9a/8e/409a8ea68a21e1c9dfe2dd567ea827ef.jpg', 'https://i.pinimg.com/originals/f1/1c/8f/f11c8f0ca66ed0e78da4ed80b5f13822.jpg','https://i.pinimg.com/originals/c8/e5/9b/c8e59b12836cfe2eca4af2f03c801400.jpg', 'https://i.ytimg.com/vi/O9h_aaT6P-k/maxresdefault.jpg'];
+  this.rand = Math.floor(Math.random()*this.myArray.length);
+  this.imageAuto = this.myArray[this.rand];
+}
+
 }
 customElements.define("show-autos", ShowAutos);
--- lit-autos.js
+++ lit-autos.js
@@ -19,17 +19,37 @@ export class LitAutos extends LitElement {
     css`
       :host {
         display: flex;
+        flex-direction:column;
+        align-items:center;
+      }
+      .registerForm {
+        background-image: url("https://media.autoexpress.co.uk/image/private/s--jf7Mv70j--/v1562251437/autoexpress/images/car_photo_259188.jpg");
+        background-size: 90vw 50vh;
+        display: flex;
+        flex-direction:column;
+        width:90vw;
+        height:40vh;
+        align-items:center;
+        font-weight:bold;
+        padding:2em;
+        font-size:1.5em;
+      }
+      .registerButton {
+        background-color:white;
+        border-radius:20px;
       }
     `,
   ];
 
   render() {
     return html`
-      <div>
+      <div class="registerForm">
+    <div>
         <label>Name: </label> <input id="autoName" /> <label>Year: </label
         ><input id="autoYear" /> <label>Brand: </label><input id="autoBrand" />
         <label>Version: </label><input id="autoVersion" />
-        <button @click="${this.saveData}">Register</button>
+        <button class="registerButton" @click="${this.saveData}">Register</button>
+        </div>
       </div>
       <show-autos
         name=${this.name}
