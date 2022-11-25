dc802dd54f7298c2431279cdb227f9cb2b2243b8
Nizcat <nizalli@gmail.com>
2022-11-25 07:12:59 -0600

    it shows the autos in grid display

--- components/show-autos.js
+++ components/show-autos.js
@@ -7,43 +7,79 @@ export default class ShowAutos extends LitElement {
     year: { type: String },
     brand: { type: String },
     version: { type: String },
-    myArray: {type: Array},
+    myArray: { type: Array },
     rand: { type: Number },
   };
 
   constructor() {
     super();
-    this.auto = [{}];
-    this.saveData();
+    this.auto = [
+      {
+        name: "Vocho",
+        year: "1969",
+        brand: "VW",
+        version: "Classic",
+        img: "https://assets.catawiki.nl/assets/2020/1/22/b/8/7/b8756b1a-0561-4f99-a2ec-edfe65842c38.jpg",
+      },
+    ];
+
     this.pickAnImage();
   }
- 
 
   static styles = [
     css`
       :host {
         display: block;
       }
+      .autoImage {
+        width: 20em;
+        height: 15em;
+        size: cover;
+      }
+      .autosContainer {
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr ;
+        grid-template-rows: 1fr 1fr 1fr ;
+        grid-gap: 2em;
+        justify-items:center;
+        
+        width: 80vw;
+      }
+      .card {
+        border: solid .5em gray
+      }
+      .autoInfo {
+        display: grid;
+        grid-template-columns: 1fr 1fr  ;
+        grid-template-rows: 1fr 1fr ;
+        justify-items:center;
+        align-items:center;
+      }
     `,
   ];
 
   render() {
-    return html` <div>${this.saveData(), this.pickAnImage()}</div>
-    <div class="" >
-    ${this.auto.map(eachAuto => 
-    html`
-  <div class= "card" >
-      <div class= "card-content">
-          <h2>${eachAuto.name}</h2>
-          <p>${eachAuto.brand}</p>
-          <img src=${eachAuto.img}>
+    return html`
+      <div>${(this.saveData(), this.pickAnImage())}</div>
+      <div class="autosContainer">
+        ${this.auto.map(
+          (eachAuto) =>
+            html`
+              <div class="card">
+                <div class="card-content">
+                  <div class="autoInfo">
+                  <h2>Name: ${eachAuto.name}</h2>
+                  <p>Brand: ${eachAuto.brand}</p>
+                  <p>Version: ${eachAuto.version}</p>
+                  <p>Year: ${eachAuto.year}</p>
+                  </div>
+                  <img class="autoImage" src=${eachAuto.img} />
+                </div>
+              </div>
+            `
+        )}
       </div>
-
-  </div>
-  `)}
-    </div>
-
-      `;
+    `;
   }
   saveData() {
     this.auto.push({
@@ -55,11 +91,18 @@ export default class ShowAutos extends LitElement {
     });
     console.log(this.auto);
   }
-  pickAnImage(){
-  this.myArray = ['https://i.pinimg.com/474x/9a/a0/5d/9aa05dfc9d9a0c0b1a32ab39a7df3a8e.jpg', 'https://i.pinimg.com/736x/92/71/60/927160803cfd068253662a304cb2efff--chevrolet-auto-chevrolet-caprice.jpg', 'https://i.pinimg.com/originals/77/ef/84/77ef8486b48c628be8f0c8316a4a6139.jpg', 'https://i.pinimg.com/originals/40/9a/8e/409a8ea68a21e1c9dfe2dd567ea827ef.jpg', 'https://i.pinimg.com/originals/f1/1c/8f/f11c8f0ca66ed0e78da4ed80b5f13822.jpg','https://i.pinimg.com/originals/c8/e5/9b/c8e59b12836cfe2eca4af2f03c801400.jpg', 'https://i.ytimg.com/vi/O9h_aaT6P-k/maxresdefault.jpg'];
-  this.rand = Math.floor(Math.random()*this.myArray.length);
-  this.imageAuto = this.myArray[this.rand];
-}
-
+  pickAnImage() {
+    this.myArray = [
+      "https://i.pinimg.com/474x/9a/a0/5d/9aa05dfc9d9a0c0b1a32ab39a7df3a8e.jpg",
+      "https://i.pinimg.com/736x/92/71/60/927160803cfd068253662a304cb2efff--chevrolet-auto-chevrolet-caprice.jpg",
+      "https://i.pinimg.com/originals/77/ef/84/77ef8486b48c628be8f0c8316a4a6139.jpg",
+      "https://i.pinimg.com/originals/40/9a/8e/409a8ea68a21e1c9dfe2dd567ea827ef.jpg",
+      "https://i.pinimg.com/originals/f1/1c/8f/f11c8f0ca66ed0e78da4ed80b5f13822.jpg",
+      "https://i.pinimg.com/originals/c8/e5/9b/c8e59b12836cfe2eca4af2f03c801400.jpg",
+      "https://i.ytimg.com/vi/O9h_aaT6P-k/maxresdefault.jpg",
+    ];
+    this.rand = Math.floor(Math.random() * this.myArray.length);
+    this.imageAuto = this.myArray[this.rand];
+  }
 }
 customElements.define("show-autos", ShowAutos);
--- lit-autos.js
+++ lit-autos.js
@@ -4,39 +4,45 @@ import "./components/show-autos";
 export class LitAutos extends LitElement {
   static properties = {
     autos: { type: Object },
-    name: {type: String},
-    year: {type: String},
-    brand: {type: String},
-    version: {type: String},
+    name: { type: String },
+    year: { type: String },
+    brand: { type: String },
+    version: { type: String },
   };
 
   constructor() {
     super();
     this.autos = [{}];
+    this.name = "Vocho";
+    this.year = "1969";
+    this.brand = "VW";
+    this.version = "Classic";
+    this.img =
+      "https://assets.catawiki.nl/assets/2020/1/22/b/8/7/b8756b1a-0561-4f99-a2ec-edfe65842c38.jpg";
   }
 
   static styles = [
     css`
       :host {
         display: flex;
-        flex-direction:column;
-        align-items:center;
+        flex-direction: column;
+        align-items: center;
       }
       .registerForm {
         background-image: url("https://media.autoexpress.co.uk/image/private/s--jf7Mv70j--/v1562251437/autoexpress/images/car_photo_259188.jpg");
         background-size: 90vw 50vh;
         display: flex;
-        flex-direction:column;
-        width:90vw;
-        height:40vh;
-        align-items:center;
-        font-weight:bold;
-        padding:2em;
-        font-size:1.5em;
+        flex-direction: column;
+        width: 90vw;
+        height: 40vh;
+        align-items: center;
+        font-weight: bold;
+        padding: 2em;
+        font-size: 1.5em;
       }
       .registerButton {
-        background-color:white;
-        border-radius:20px;
+        background-color: white;
+        border-radius: 20px;
       }
     `,
   ];
@@ -44,11 +50,14 @@ export class LitAutos extends LitElement {
   render() {
     return html`
       <div class="registerForm">
-    <div>
-        <label>Name: </label> <input id="autoName" /> <label>Year: </label
-        ><input id="autoYear" /> <label>Brand: </label><input id="autoBrand" />
-        <label>Version: </label><input id="autoVersion" />
-        <button class="registerButton" @click="${this.saveData}">Register</button>
+        <div>
+          <label>Name: </label> <input id="autoName" /> <label>Year: </label
+          ><input id="autoYear" /> <label>Brand: </label
+          ><input id="autoBrand" /> <label>Version: </label
+          ><input id="autoVersion" />
+          <button class="registerButton" @click="${this.saveData}">
+            Register
+          </button>
         </div>
       </div>
       <show-autos
