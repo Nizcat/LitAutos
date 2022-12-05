7c5822b7e3cf1d57d1aeeefad4dd25de63796bff
Nizcat <nizalli@gmail.com>
2022-12-05 16:01:46 -0600

    filter functions and filtered autos are shown

--- lit-autos.js
+++ lit-autos.js
@@ -14,6 +14,7 @@ export class LitAutos extends LitElement {
     theAutos: { type: Object },
     tofilterAutos: { type: Object },
     list: { type: Array },
+    filteredAutos: { type: Object },
   };
 
   constructor() {
@@ -27,6 +28,7 @@ export class LitAutos extends LitElement {
     this.showFilteredInfo = false;
     this.theAutos = [{}];
     this.list = ["options"];
+    this.filteredAutos = [{}];
 
     this.addEventListener("nameValue", (e) => {
       if (e.detail["value"][0] == "Name :") {
@@ -41,9 +43,8 @@ export class LitAutos extends LitElement {
     });
 
     this.addEventListener("theAutos", (e) => {
-     
       this.theAutos = e.detail["autos"];
-      
+      this.sortingAutos();
     });
   }
 
@@ -77,9 +78,37 @@ export class LitAutos extends LitElement {
         font-size: 1.5em;
       }
       .filter {
-        width:15em;
+        width: 15em;
         border: 3px solid blue;
-        color:black;
+        color: black;
+      }
+      .autoImage {
+        width: 20em;
+        height: 15em;
+        size: cover;
+      }
+      .autosContainer {
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr;
+        grid-template-rows: 1fr 1fr 1fr;
+        grid-gap: 2em;
+        margin-top: 2em;
+        justify-items: center;
+
+        width: 80vw;
+      }
+      .card {
+        background-color: white;
+        padding: 1em;
+        box-shadow: 0.5em 0.5em #d7d5d5;
+      }
+      .autoInfo {
+        height: fit-content;
+        display: grid;
+        grid-template-columns: 60% 40%;
+        grid-template-rows: 3em 2em;
+        justify-items: center;
+        align-items: center;
       }
     `,
   ];
@@ -87,34 +116,65 @@ export class LitAutos extends LitElement {
   render() {
     return html`
       ${this.showFilteredInfo
-        ? html` <p>probando true</p> `
+        ? html` <div class="autosContainer">
+            ${this.filteredAutos.map(
+              (eachAuto) =>
+                html`
+                <div class="card">
+                  <div class="card-content">
+                    <div class="autoInfo">
+                      <h2>${eachAuto.name}</h2>
+                      <p>Brand: ${eachAuto.brand}</p>
+                      <p>Version: ${eachAuto.version}</p>
+                      <p>Year: ${eachAuto.year}</p>
+                    </div>
+                    <img class="autoImage" src=${eachAuto.img} />
+                  </div>
+                </div>${console.log(this.showFilteredInfo, "booleano")}
+                </div>
+              `
+            )}
+          </div>`
         : html` <div class="registerForm">
-            <input-info inputName="autoName" labelName="Name :"></input-info>
-            <input-info inputName="autoYear" labelName="Year :"> </input-info>
-            <input-info inputName="autoBrand" labelName="Brand :"></input-info>
-            <input-info
-              inputName="autoVersion"
-              labelName="Version :"
-            ></input-info>
+              <input-info inputName="autoName" labelName="Name :"></input-info>
+              <input-info inputName="autoYear" labelName="Year :"> </input-info>
+              <input-info
+                inputName="autoBrand"
+                labelName="Brand :"
+              ></input-info>
+              <input-info
+                inputName="autoVersion"
+                labelName="Version :"
+              ></input-info>
 
-            <button
-              id="registerButton"
-              class="registerButton"
-              @click="${this.saveData}"
-            >
-              Register
-            </button>
-          </div>`}
-      <!-- aquí va un renderizado condicional con el filtro, el componente filtro eleva un valor true al componente padre para el renderizado condicional y el componente hijo aparece y pinta los autos del filtro-->
-      <div id="filterContainer">
-        ${this.sortingAutos()}
-      </div>
-      <show-autos
-        name=${this.name}
-        year=${this.year}
-        brand=${this.brand}
-        version=${this.name}
-      ></show-autos>
+              <button
+                id="registerButton"
+                class="registerButton"
+                @click="${this.saveData}"
+              >
+                Register
+              </button>
+            </div>
+
+            <div>
+              <select
+                id="filterContainer"
+                class="filter"
+                name="filter"
+                @change=${this.getItem}
+              >
+                ${this.list.map(
+                  (eachAuto) =>
+                    html`<option value=${eachAuto}>${eachAuto}</option>`
+                )}
+              </select>
+            </div>
+            <show-autos
+              name=${this.name}
+              year=${this.year}
+              brand=${this.brand}
+              version=${this.name}
+            ></show-autos>`}
     `;
   }
   saveData() {
@@ -126,25 +186,24 @@ export class LitAutos extends LitElement {
   }
 
   sortingAutos() {
-    return html`
-    <select class="filter">
-      ${(this.select_list = Object.values(this.theAutos).map((auto) => {
-        if (!this.list.includes(auto.name) && auto.name != "") {
-          this.list.push(auto.name);
-          console.log(this.list, "antes dde pintar nombres");
+    this.filter = this.shadowRoot.getElementById("filterContainer");
+    this.select_list = Object.values(this.theAutos).map((auto) => {
+      if (!this.list.includes(auto.name) && auto.name != "") {
+        this.list.push(auto.name);
+      }
+    });
+    this.requestUpdate();
+  }
+  getItem() {
+    this.showFilteredInfo = true;
+    this.item = this.shadowRoot.getElementById("filterContainer").value;
+    for (this.auto of this.theAutos) {
+      if (this.auto.name === this.item) {
+        this.filteredAutos.push(this.auto);
+      }
+    }
 
-          this.list.map(
-            (element) =>
-              html`${console.log(element)}
-                <option>${element}</option> `
-          );
-        }
-      }))}
-    </select>
-    ${this.requestUpdate()}
-  `;
- 
-    
+    this.requestUpdate();
   }
 }
 customElements.define("lit-autos", LitAutos);
